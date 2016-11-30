b81553c510955e7449759241021d95a7d3ab8654
Jacob Froman <jacob.h.froman@gmail.com>
2016-11-30 14:48:25 -0600

    Added more country info to DOM

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,14 @@ $(document).ready(function() {
     $('#countrySelect').on('change', function() {
       showSelectedCountryInfo();
     });
-    
+
     $.ajax({
         url: "http://devjana.net/support/countries.json",
         dataType: 'JSON',
         success: function(data) {
           console.log(data);
           data.countries.forEach(function(country) {
-            displayCountry(country);
+            addCountryToSelect(country);
           });
         },
         statusCode: {
@@ -22,7 +22,7 @@ $(document).ready(function() {
     });
 });
 
-function displayCountry(country) {
+function addCountryToSelect(country) {
   $('#countrySelect').append(
     '<option value="' + country.code + '">' + country.name + '</option>'
   );
@@ -35,6 +35,36 @@ function showSelectedCountryInfo() {
     dataType: 'JSON',
     success: function(data) {
       console.log(data);
+      displayCountryInfo(data);
     }
   });
+}
+
+function displayCountryInfo(country) {
+  var htmlString = '<div class="country">';
+  // Country title with name, native name, and population
+  htmlString += '<h3>' + country.name + ', natively known as ' + country.nativeName;
+  htmlString += ', has a population of ' + country.population + '</h3>';
+  // List of additional info
+  htmlString += '<ul><li>The capital of ' + country.name + ' is ' + country.capital + '</li>';
+  htmlString += '<li>It has an area of ' + country.area + '</li>';
+  htmlString += '<li>' + buildBorderString(country.borders) + '</li>';
+  htmlString += '</ul>';
+
+  $('#countryDisplay').html(htmlString);
+}
+
+function buildBorderString(borders) {
+  var borderString = 'It is bordered by ';
+  if (borders.length > 0) {
+    borders.forEach(function(country) {
+      console.log(country);
+      borderString += country + ', ';
+    });
+    borderString = borderString.substring(0, borderString.length - 2);
+    borderString += '.';
+  } else {
+    borderString = 'It has no bordering countries.';
+  }
+  return borderString;
 }
